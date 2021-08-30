3f663116c7ff1775712fae13487dbe1621178793
jesseagleboy <jslomowitz@gmail.com>
2021-08-30 03:06:59 -0400

    access token method created

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,4 +1,5 @@
 let userAccessToken;
+const clientID = ''
 
 const getAccessToken = () => {
     if (userAccessToken) {
@@ -6,7 +7,17 @@ const getAccessToken = () => {
     }
 
     const accessToken = window.location.href.match(/access_token=([^&]*)/);
-    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/); 
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (accessToken && expiresInMatch) {
+        userAccessToken = accessToken[1];
+        const expirationTime = Number(expiresInMatch[1]);
+
+        window.setTimeout(() => userAccessToken = '', expirationTime * 1000);
+        window.history.pushState('Access Token', null, '/');
+
+        return accessToken;
+    }
 }
 
 const Spotify = {
