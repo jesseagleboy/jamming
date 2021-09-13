d333c9413a78f9557fa29dee2853ce8c4a26639e
jesseagleboy <jslomowitz@gmail.com>
2021-09-13 10:50:31 -0400

    new url

--- src/util/clientInfo.js
+++ src/util/clientInfo.js
@@ -1,6 +1,6 @@
 const info = {
     clientID: '45ac95ecddbe49f08cebc41be6957d77',
-    redirectURI: 'http://localhost:3000/'
+    redirectURI: 'https://music-jamming.herokuapp.com/'
 }
 
 export default info;
